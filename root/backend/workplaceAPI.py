ee448486ac806bce36bbcb6e1474ac91c6e3e1a5
TylerMG17 <tylermg17@gmail.com>
2023-08-04 16:40:43 +1000

    Updated get workplace api

--- root/backend/workplaceAPI.py
+++ root/backend/workplaceAPI.py
@@ -1,6 +1,6 @@
 from backend import app
 
 # setting up a basic workplace, "Dont know if this is exactly what you wanted @TylerMG2"
-@app.route("/workplace")
-def get_workplaces():
-    return "These are some workplaces!!"
+@app.route("/workplace/<id>")
+def get_workplace(id: str):
+    return "Here is a workplace with the id: " + id
