9601dbbc48538188321edba7ebddbd07fff0cc01
TylerMG17 <tylermg17@gmail.com>
2023-08-04 16:39:31 +1000

    Added more shift endpoints

--- root/backend/__init__.py
+++ root/backend/__init__.py
@@ -1,14 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 # Create app
 app = Flask(__name__)
 
+# Import views
 import backend.shiftAPI
 
 # Main index
 @app.route("/")
 def hello_world():
     return "Hello, World!"
-
-# Import blueprints
-#import shiftAPI
--- root/backend/shiftAPI.py
+++ root/backend/shiftAPI.py
@@ -1,6 +1,21 @@
+from flask import request
 from backend import app
 
-# Get shift at given workplace
-@app.route("/shifts")
-def get_shifts():
-    return "Heres some shifts"
+# Return all the shifts in a workplace
+@app.route("/shifts/<id>")
+def get_shifts(id: str):
+    return "Heres some shifts from the workplace with the id: " + id 
+
+# Add a shift to the database
+@app.route("/shifts", methods=['POST'])
+def add_shift():
+    details = request.get_json()
+    return "Adding the shift with the details: " + details
+
+# Get the details about a specific shift
+@app.route("/shift/<id>")
+def get_shift(id: str):
+
+    # Return the shift
+    return "Here is the shift with the id: " + id
+
